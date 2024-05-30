990f54be5e2506c3c2f52ee075563c46ac5ffbba
CCF2P <yura.smolyaninov.01@mail.ru>
2024-05-30 19:17:02 +1000

    Ready for tests

--- Core/CoreController.py
+++ Core/CoreController.py
@@ -8,15 +8,18 @@ from Core.Databases.DB_Modules.TuzovAnalyzer.main import main
 class CoreController():
     def __init__(self) -> None:
         self._db_controller = DatabaseController()
-        
+    
+    # Function for start Tuzov analyzer
     def run_analyzer(self, path: str=None):
         if path is None:
             return main()
         return main(path)
 
+    # Function that update information about dictionaries in database
     def _update_dict_list(self) -> None:
         self._db_controller._update_dict_list()
 
+    # Function that update information about modules in database
     def _update_modules_list(self) -> None:
         self._db_controller._update_modules_list()
 
@@ -26,6 +29,7 @@ class CoreController():
     def get_dictionaries(self) -> list[str]:
         return self._db_controller.get_dictionaries()
     
+    # Run module by calling the script via sys
     def run_module(self, name: str) -> None:
         mod_path = self._db_controller.get_module_path(name)
         if mod_path == "None":
